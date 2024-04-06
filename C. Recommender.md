e36f2b8f434744ffba436f65c68aa6769fecf434
Adrià <nouacl@gmail.com>
2024-04-07 01:27:11 +0200

    recommender started

---
+++ C. Recommender.md
@@ -0,0 +1,10 @@
+We start by creating a recommender for the database community, given by the keywords:
+- data management
+- indexing
+- data modeling
+- big data
+- data processing
+- data storage
+- data querying
+### Finding papers from the database community
+ There are no papers with any of the previous keywords assigned. Hence, let us dda
---
+++ C.Recommender.py
@@ -0,0 +1,23 @@
+from neo4j import GraphDatabase
+
+URI = "bolt://localhost:7687"
+AUTH = ("neo4j", "123456789")
+db = 'neo4j'
+
+# Run queries
+with GraphDatabase.driver(URI, auth=AUTH) as driver:
+    try:
+        # 1. Find the top 3 most cited papers of each conference.
+        records, summary, keys = driver.execute_query('''
+            ''', database_=db
+                                                      )
+        for record in records:
+            pass
+            # print(record)
+        print("The query `{query}` returned {records_count} records in {time} ms.".format(
+            query=summary.query, records_count=len(records),
+            time=summary.result_available_after
+        ))
+    except Exception as e:
+        print('\nException raised:')
+        print(e)
