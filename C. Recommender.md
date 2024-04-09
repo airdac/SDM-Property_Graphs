0f5c999473d90c7b8e8d2bd10b6831e288782c88
Adrià <nouacl@gmail.com>
2024-04-09 20:54:43 +0200

    2nd step recommender

--- C. Recommender.md
+++ C. Recommender.md
@@ -11,7 +11,7 @@ We start by creating a recommender for the database community, given by the keyw
 # Following the steps to create a recommender
 ## 1.  Define the database community
 ```cypher
-MERGE (:Research_community {name: 'database'})
+MERGE (r:Research_community {name: 'database'})
 ```
 ```cypher
 MATCH (k:Keyword)
@@ -19,3 +19,22 @@ WHERE k.tag IN ['data management', 'indexing', 'data modeling', 'big data', 'dat
 MATCH (r:Research_community)
 MERGE (k)-[:From_r]->(r)
 ```
+## 2. Find conferences and journals related to the database community
+Nº of papers per conference/journal
+```cypher
+MATCH (cj:Conference|Journal)<-[:From_c|From_j]-(p:Paper)
+RETURN cj.name, COUNT(p)
+```
+Final query that creates a relationship *Related_to* between conferences/journals and the database community
+```cypher
+// Match conferences/journals with papers
+MATCH (cj:Conference|Journal)<-[]-(:Edition|Volume)<-[]-(p:Paper)
+WITH cj, COUNT(p) AS n_papers
+// Match conferences/journals with database papers
+MATCH (cj)<-[]-(:Edition|Volume)<-[]-(db_p:Paper)<-[]-(:Keyword)-[]->(r:Research_community {name: 'database'})
+WITH r, cj, n_papers, COUNT(db_p) AS n_db_papers
+WITH r, cj, 1.0 * n_db_papers/ n_papers AS percentage_db_papers
+WHERE percentage_db_papers >= 0.9
+MERGE (cj)-[:Related_to]->(r)
+```
+## 3. Top papers of each conference/journal
--- property_graphs/C_Recommender.py
+++ property_graphs/C_Recommender.py
@@ -5,7 +5,6 @@ from query_execution import *
 
 URI = "bolt://localhost:7687"
 AUTH = ("neo4j", "123456789")
-db = 'neo4j'
 
 OUT = Path(__file__).parent.parent.absolute()/'C_output'
 OUT.mkdir(exist_ok=True)
@@ -15,7 +14,7 @@ with GraphDatabase.driver(URI, auth=AUTH) as driver:
     try:
         driver.verify_connectivity()
         execute_print(driver, '''
-            MERGE(: Research_community {name: 'database'})
+            MERGE(r: Research_community {name: 'database'})
                     ''')
         execute_print(driver, '''
             MATCH(k: Keyword)
@@ -23,6 +22,17 @@ with GraphDatabase.driver(URI, auth=AUTH) as driver:
             MATCH(r: Research_community)
             MERGE(k)-[:From_r] -> (r)
                     ''')
+        execute_print(driver, '''
+            // Match conferences/journals with papers
+            MATCH (cj:Conference|Journal)<-[]-(:Edition|Volume)<-[]-(p:Paper)
+            WITH cj, COUNT(p) AS n_papers
+            // Match conferences/journals with database papers
+            MATCH (cj)<-[]-(:Edition|Volume)<-[]-(db_p:Paper)<-[]-(:Keyword)-[]->(r:Research_community {name: 'database'})
+            WITH r, cj, n_papers, COUNT(db_p) AS n_db_papers
+            WITH r, cj, 1.0 * n_db_papers/ n_papers AS percentage_db_papers
+            WHERE percentage_db_papers >= 0.9
+            MERGE (cj)-[:Related_to]->(r)
+                      ''')
     except Exception as e:
         print('\nException raised:')
         print(e)
